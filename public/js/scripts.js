ceea8dfa7cd8c2d6329ecc66040164ba38040262
MirzaMaulana <mirzaartstudio35@gmail.com>
2023-03-14 10:52:20 +0700

    UPDATE

---
+++ public/js/scripts.js
@@ -0,0 +1,8 @@
+// Year at the bottom
+// Typing Text
+var Typed = new Typed(".write", {
+  strings: ["Student", "Software Engineering", "Front End Devoloper"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
